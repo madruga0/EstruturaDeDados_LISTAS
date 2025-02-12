d9fd209e6ebeb6fa25881a30af454390522a3075
Pedro Madruga <pedromadruga6@gmail.com>
2025-02-12 09:51:30 -0300

    Added many files

--- edu/listasencadeadas/ListaEncadeada.java
+++ edu/listasencadeadas/ListaEncadeada.java
@@ -7,19 +7,37 @@ public class ListaEncadeada<T> {
         this.referenciaEntrada = null;
     }
 
+    public void add(T conteudo) {
+        No<T> novoNo = new No<>(conteudo);
+        if (this.isEmpty()) {
+            referenciaEntrada = novoNo;
+            return;
+        }
+
+        No<T> noAuxiliar = referenciaEntrada;
+        for (int i = 0; i < this.size() - 1; i++) {
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+
+        noAuxiliar.setProximoNo(novoNo);
+    }
+
     public int size() {
         int tamanhoLista = 0;
         No<T> referenciaAux = referenciaEntrada;
-        while(true) {
-            if(referenciaAux != null) {
+        while (true) {
+            if (referenciaAux != null) {
                 tamanhoLista++;
                 if (referenciaAux.getProximoNo() != null) {
-                    
+                    referenciaAux = referenciaAux.getProximoNo();
+                } else {
+                    break;
                 }
-            }else {
+            } else {
                 break;
             }
         }
+        return tamanhoLista;
     }
 
     public boolean isEmpty() {
